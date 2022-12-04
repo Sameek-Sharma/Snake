66d5a298c203306fde2e363fd28e7923e2eb9cbf
mmbouzan <116596604+mmbouzan@users.noreply.github.com>
2022-12-04 13:38:46 -0500

    Update SnakeChange.py

--- SnakeChange.py
+++ SnakeChange.py
@@ -8,39 +8,39 @@
 # Melanie Bouzanne- MB
 
 import random, pygame, sys
-from pygame.locals import * #imports pygame and all of the neccessary functions that are neccessary to run the game
+from pygame.locals import * 
 
-FPS = 10 #Sets Speed, CHANGED FROM 15 TO 10
-WINDOWWIDTH = 1440 #game window width (x-direction), CHANGED FROM 640 to 
-WINDOWHEIGHT = 1080 #game window height (y-direction), CHANGED FROM 480 TO 1080
-CELLSIZE = 20 #size value for individual square
-assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size." #ensures correct aspect ratio for window
-assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size." #ensures correct aspect ratio for window
-CELLWIDTH = int(WINDOWWIDTH / CELLSIZE) #finds int value of squares in horizontal
-CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE) #finds int value of squares in vertical
+FPS = 10 # CHANGED FROM 15 TO 10
+WINDOWWIDTH = 1440 # CHANGED FROM 640 to 
+WINDOWHEIGHT = 1080 # CHANGED FROM 480 TO 1080
+CELLSIZE = 20 
+assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size." 
+assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size." 
+CELLWIDTH = int(WINDOWWIDTH / CELLSIZE) 
+CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE) 
 
 #             R    G    B
-WHITE     = (255, 255, 255) #RGB value of white
-BLACK     = (  0,   0,   0) #RGB value of black
-RED       = (255,   0,   0) #RGB value of red
-GREEN     = (  0, 255,   0) #RBG value of green (lighter shade)
-DARKGREEN = (  0, 155,   0) #RGB value of green (darker shade
-DARKGRAY  = ( 40,  40,  40) #RGB value of grey (lighter black)
-BGCOLOR = BLACK #game background is black
+WHITE     = (255, 255, 255) 
+BLACK     = (  0,   0,   0) 
+RED       = (255,   0,   0) 
+GREEN     = (  0, 255,   0) 
+DARKGREEN = (  0, 155,   0) 
+DARKGRAY  = ( 40,  40,  40) 
+BGCOLOR = BLACK 
 
 UP = 'up' 
 DOWN = 'down' 
 LEFT = 'left' 
 RIGHT = 'right' 
 
-HEAD = 0 # syntactic sugar: index of the worm's head
+HEAD = 0 
 
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT
 
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT)) #displays the game window with the specified dimensions
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT)) 
     BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
     pygame.display.set_caption('Wormy')
 
@@ -48,11 +48,11 @@ def main():
     while True:
         runGame()
         showGameOverScreen()
-#above function ->  Opens the game and shows the window over the current on (sets priority), sets a clock animation for start screen, display surface (new window), fonts (to display wording and numbers), and sets window caption (name of the open window running on the computer)
+
 
 
 def runGame():
-    # Set a random start point.
+
     startx = random.randint(5, CELLWIDTH - 6)
     starty = random.randint(5, CELLHEIGHT - 6)
     wormCoords = [{'x': startx,     'y': starty},
@@ -60,40 +60,39 @@ def runGame():
                   {'x': startx - 2, 'y': starty}]
     direction = RIGHT
 
-    # Start the apple in a random place.
+    
     apple = getRandomLocation()
 
-    while True: # main game loop
-        for event in pygame.event.get(): # event handling loop
+    while True: 
+        for event in pygame.event.get(): 
             if event.type == QUIT:
                 terminate()
             elif event.type == KEYDOWN: 
                 if (event.key == K_LEFT or event.key == K_a) and direction != RIGHT:
-                    direction = LEFT #when left button is pressed, move left
+                    direction = LEFT 
                 elif (event.key == K_RIGHT or event.key == K_d) and direction != LEFT:
-                    direction = RIGHT #when right button is pressed, move left
+                    direction = RIGHT 
                 elif (event.key == K_UP or event.key == K_w) and direction != DOWN:
-                    direction = UP #when up button is pressed, move up
+                    direction = UP 
                 elif (event.key == K_DOWN or event.key == K_s) and direction != UP:
-                    direction = DOWN #when down button is pressed, move down
+                    direction = DOWN 
                 elif event.key == K_ESCAPE:
-                    terminate() #when escape is pressed, terminate
-                #purpose of previous 5 statements -> assign keybinds for the game
-        # check if the worm has hit itself or the edge
+                    terminate()
+                
+        
         if wormCoords[HEAD]['x'] == -1 or wormCoords[HEAD]['x'] == CELLWIDTH or wormCoords[HEAD]['y'] == -1 or wormCoords[HEAD]['y'] == CELLHEIGHT:
-            return # game over
+            return 
         for wormBody in wormCoords[1:]:
             if wormBody['x'] == wormCoords[HEAD]['x'] and wormBody['y'] == wormCoords[HEAD]['y']:
-                return # game over
+                return 
 
-        # check if worm has eaten an apply
+        
         if wormCoords[HEAD]['x'] == apple['x'] and wormCoords[HEAD]['y'] == apple['y']:
-            # don't remove worm's tail segment
-            apple = getRandomLocation() # set a new apple somewhere
+            apple = getRandomLocation()
         else:
-            del wormCoords[-1] # remove worm's tail segment
+            del wormCoords[-1] 
 
-        # move the worm by adding a segment in the direction it is moving
+      
         if direction == UP:
             newHead = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] - 1}
         elif direction == DOWN:
@@ -116,7 +115,7 @@ def drawPressKeyMsg():
     pressKeyRect = pressKeySurf.get_rect()
     pressKeyRect.topleft = (WINDOWWIDTH - 200, WINDOWHEIGHT - 30)
     DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
-#above function -> design and print of message prompting the viewer to enter a key to start the game
+
 
 def checkForKeyPress():
     if len(pygame.event.get(QUIT)) > 0:
@@ -128,13 +127,13 @@ def checkForKeyPress():
     if keyUpEvents[0].key == K_ESCAPE:
         terminate()
     return keyUpEvents[0].key
-#above function -> checks for a key press and does what the key requests: quit the game when the quit button is pressed, 
+ 
 
 def showStartScreen():
     titleFont = pygame.font.Font('freesansbold.ttf', 100)
     titleSurf1 = titleFont.render('Wormy!', True, WHITE, DARKGREEN)
     titleSurf2 = titleFont.render('Wormy!', True, GREEN)
-#above funciton -> Purpose: show start screen (what is displayed when the window is opened?), It will display <Wormy!> in white, darkgreeen, and green (colours defined earlier)
+
     
     degrees1 = 0
     degrees2 = 0
@@ -153,22 +152,22 @@ def showStartScreen():
         drawPressKeyMsg()
 
         if checkForKeyPress():
-            pygame.event.get() # clear event queue
+            pygame.event.get() 
             return
         pygame.display.update()
         FPSCLOCK.tick(FPS)
-        degrees1 += 3 # rotate by 3 degrees each frame
-        degrees2 += 7 # rotate by 7 degrees each frame
+        degrees1 += 3
+        degrees2 += 7 
 
 
 def terminate():
     pygame.quit()
     sys.exit()
-#above function -> terminates the game and window when the user inputs that the game should be closed
 
+    
 def getRandomLocation():
     return {'x': random.randint(0, CELLWIDTH - 1), 'y': random.randint(0, CELLHEIGHT - 1)}
-#above function -> gets a random number that lies on the playing field for different applications
+
 
 def showGameOverScreen():
     gameOverFont = pygame.font.Font('freesansbold.ttf', 150)
@@ -184,20 +183,20 @@ def showGameOverScreen():
     drawPressKeyMsg()
     pygame.display.update()
     pygame.time.wait(500)
-    checkForKeyPress() # clear out any key presses in the event queue
+    checkForKeyPress() 
 
     while True:
         if checkForKeyPress():
-            pygame.event.get() # clear event queue
+            pygame.event.get() 
             return
-#above function -> sets the <game over> visual with colour, size and position, clears key press que, and waits for a new key input (to restart game)
+
         
 def drawScore(score):
     scoreSurf = BASICFONT.render('Score: %s' % (score), True, WHITE)
     scoreRect = scoreSurf.get_rect()
     scoreRect.topleft = (WINDOWWIDTH - 120, 10)
     DISPLAYSURF.blit(scoreSurf, scoreRect)
-#above function -> sets scoreboard visual with colour, size, and position
+
 
 def drawWorm(wormCoords):
     for coord in wormCoords:
